254e16fa6f654d75a9530436d7b5057ce87f527c
GasparAcevedoZainSouthworks <gaspar.acevedo@southworks.com>
2018-10-12 09:35:51 -0300

    WebAPI project added

---
+++ NET_WebAPI/NET_WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using NET_WebAPI.Models;
+using NET_WebAPI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NET_WebAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private ProductRepository repository;
+        private IEnumerable<Product> products;
+
+        private ProductsController()
+        {
+            repository = ProductRepository.GetRepository();
+            products = repository.GetProducts();
+        }
+
+        [ActionName("GetAll")]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int Id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == Id);
+            if (product == null)
+                return NotFound();
+            return Ok(product);
+        }
+    }
+}
---
+++ NET_WebAPI/NET_WebAPI/MockedRepositories/ProductRepository.cs
@@ -0,0 +1,47 @@
+﻿using NET_WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NET_WebAPI.Repositories
+{
+    public class ProductRepository
+    {
+        private static ProductRepository repository = null;
+        private List<Product> products;
+
+        private ProductRepository()
+        {
+            SetProducts();
+        }
+
+        private void SetProducts()
+        {
+            products = new List<Product>();
+
+            for (int i = 1; i <= 20; i++)
+            {
+                products.Add(new Product
+                {
+                    Id = i,
+                    Name = "Product_" + i,
+                    Category = "Category_" + (i % 10),
+                    Price = i * 10 + ((decimal)i / 10)
+                });
+            }
+        }
+
+        public static ProductRepository GetRepository()
+        {
+            if (repository == null)
+                repository = new ProductRepository();
+            return repository;
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return products;
+        }
+    }
+}
